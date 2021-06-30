01b108597fc5ffb1242f91a939096d1e5bc69526
Liinal <liina@lumoslabs.com>
2021-06-30 16:02:19 -0700

    unused widecheckbox

--- src/styles/colors/base.js
+++ src/styles/colors/base.js
@@ -1,11 +1,12 @@
 export const lumosBlack = '#333333'; // aka grayDark
 export const lumosWhite = '#FFFFFF';
 export const lumosOrange = '#F1693C';
+export const orange6 = '#C94727'; // aka darkOrange
 
 const base = {
   lumosBlack: lumosBlack,
   lumosWhite: lumosWhite,
-  lumosOrange: lumosOrange
+  lumosOrange: lumosOrange,
 };
 
 export default base;
