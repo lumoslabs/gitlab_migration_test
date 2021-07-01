4d7e50e3fdf4bb248dc9db8c3c7cf25cd5239da9
Liinal <liina@lumoslabs.com>
2021-06-30 17:04:28 -0700

    Spacing, commas

--- src/components/ui/GameProgressBar.tsx
+++ src/components/ui/GameProgressBar.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import { css, StyleSheet } from 'aphrodite/no-important';
@@ -22,18 +21,18 @@ const GameProgressBar = (props: IGameProgressBarProps): JSX.Element => {
 
 const styles = StyleSheet.create({
   progressBarDiv: {
-    position: 'absolute',
+    position: 'absolute'
   },
   progressOverride: {
     width: '64vmin',
     height: '3vmin',
-    borderRadius: '1.5vmin',
+    borderRadius: '1.5vmin'
   },
   loadingText: {
     color: 'rgb(0, 0, 0)',
     fontSize: '3vmin',
     fontFamily: 'MuseoSans500',
-    marginTop: '2.5vmin',
+    marginTop: '2.5vmin'
   }
 });
 
