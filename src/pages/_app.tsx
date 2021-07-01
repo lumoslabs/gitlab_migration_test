45ea52f982bf2f512ff32983663d52cdfe2f9292
Liinal <liina@lumoslabs.com>
2021-07-01 10:44:03 -0700

    bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import 'normalize.css'
 import '@styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import NestHandler from '@components/NestHandler'
 import Layout from '@components/ui/Layout'
 import LoadingComponent from '@components/ui/LoadingComponent'
+import GameProgressBar from '@components/ui/GameProgressBar'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -15,6 +17,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <LoadingComponent title=""/>
       <NestHandler />
       <Component {...pageProps} />
+      <GameProgressBar name='Train of Thought' progressLevel={50} />
     </Layout>
   )
 }
