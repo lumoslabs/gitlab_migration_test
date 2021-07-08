ccc652f626980ecccd26ada5866dbb9787258cd7
Liinal <liina@lumoslabs.com>
2021-07-08 16:25:42 -0700

    commented test, user bar test

--- src/test/components/UserInfo.test.tsx
+++ src/test/components/UserInfo.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserInfo from '@components/ui/UserInfo'
 import { render } from '@testing-library/react'
+// import renderer from 'react-test-renderer'
 
 const handleClick = () => { window.location.href = '/' }
 
@@ -17,3 +18,23 @@ it('matches snapshot', () => {
   )
   expect(asFragment()).toMatchSnapshot()
 })
+
+
+// test('UserBar displays the correct info', () => {
+//   const component = renderer.create(
+//     <UserInfo
+//       show={true}
+//       handleClose={handleClick}
+//       logoutCallback={handleClick}
+//       email='email@example.com'
+//       name='Lumosity User'
+//       timezone='Pacific Time'
+//     />, {}
+//   )
+
+//   let tree = component.toJSON()
+//   expect(tree).toMatchSnapshot()
+
+//   tree = component.toJSON()
+//   expect(tree).toMatchSnapshot()
+// })
