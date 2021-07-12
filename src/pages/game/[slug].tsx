0841ff5352787aee552dc28f2241690157e9c389
Liinal <liina@lumoslabs.com>
2021-07-12 16:01:22 -0700

    Snapshots and game results

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -3,26 +3,63 @@ import { GetServerSideProps } from 'next'
 import { GameConfig } from '@backend/models/config'
 import ConfigService from '@backend/services/ConfigService'
 import GameContainer from '@components/ui/GameContainer'
+import GameScoreCard from '@components/ui/GameScoreCard'
 
 export default function Game({ game }: { game: GameConfig }): JSX.Element {
   const [result, setResult] = useState(null)
   const onComplete = (data: any) => {
     setResult(data)
   }
+
+  // TODO: make this bring you to next game/return to Home
+  const actionButtonHandler = () => { window.location.href = 'https://lumos-assistant.ngrok.io' }
+
+  // TODO: handle data for these
+  const showTrainingIcon = true
+  const trainingIcon = '/assets/training_1by3.svg'
+  const showTrophy = true
+  const topScoreTodaysScoreIndex = 0
+  const topScoresData = [{"updated_at":"05/02/2020","score":600000}, {"updated_at":"2021-07-02T20:15:27Z","score":55000}, {"updated_at":"07/01/2020","score":5000}, {"updated_at":"07/01/2020","score":4000}, {"updated_at":"07/01/2020","score":3000}]
+  const actionButtonText = 'Next Game'
+
+  // Data from Game Config
+  const title = game.values.title
+  const gameIcon = game.values.score_thumbnail_url
+  // shall we simplify this data structure in the database?
+  const statLabel = game.values.frontend_data.scores[0].score_screen_score_key.replace('scoreScreen','')
+
+  // Data from game result
+  const currentScore = result?.score
+  const stat = result?.stat
+
+  // Is there a better way?
+  const currentPage = window.location.href
+
   return (
     <main>
-      {
-        result &&
-        JSON.stringify(result)
-        //TODO: show score card
-      }
-      {
-        (!result) &&
+      {result && (
+        <GameScoreCard
+          title={title}
+          gameIcon={gameIcon}
+          showTrainingIcon={showTrainingIcon}
+          showTrophy={showTrophy}
+          trainingIcon={trainingIcon}
+          currentScore={currentScore}
+          topScoreTodaysScoreIndex={topScoreTodaysScoreIndex}
+          topScoresData={topScoresData}
+          actionButtonText={actionButtonText}
+          actionButtonHandler={actionButtonHandler}
+          currentPage={currentPage}
+          stat={stat}
+          statLabel={statLabel}
+        />
+      )}
+      {(!result) && (
         <GameContainer
           game={game}
           onComplete={onComplete}
         />
-      }
+      )}
     </main>
   )
 }
--- src/test/components/GameScoreCard.test.tsx
+++ src/test/components/GameScoreCard.test.tsx
@@ -8,11 +8,10 @@ it('matches snapshot', () => {
   const { asFragment } = render(
     <GameScoreCard
       title='Color Match'
-      description='description'
       gameIcon='https://assets-staging.nest.lumosity.com/frontend_assets/thumbnails/medium_thumbnail_color_match.png'
       showTrainingIcon={true}
       showTrophy={true}
-      trainingIcon='assets/training_1by3.svg'
+      trainingIcon='/assets/training_1by3.svg'
       currentScore={100000}
       topScoreTodaysScoreIndex={0}
       topScoresData={[{"updated_at":"05/02/2020","score":600000}, {"updated_at":"2021-07-02T20:15:27Z","score":55000}, {"updated_at":"07/01/2020","score":5000}, {"updated_at":"07/01/2020","score":4000}, {"updated_at":"07/01/2020","score":3000}]}
