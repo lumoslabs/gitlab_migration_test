0142a4584ad389120e6bbff7f0d25029fffa7315
Yura Borue <yura.borue@gmail.com>
2021-07-14 20:31:41 +0300

    agegate func style

--- src/components/ui/AgeGate.tsx
+++ src/components/ui/AgeGate.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DatePicker from 'react-mobile-datepicker'
 import { StyleSheet, css } from 'aphrodite'
 import { Container, Row } from 'react-bootstrap'
@@ -34,94 +34,75 @@ export interface IAgeGateProps {
   buttonStyles?: any;
 }
 
-// const AgeGate = (({ onClick = () => void (0), text, buttonStyles }: IAgeGateProps): JSX.Element => {
-//   return (
-//     <button className={css([styles.button, buttonStyles])} onClick={onClick} >
-//       {text}
-//     </button>
-//   )
-// })
 
-class AgeGate extends React.Component {
-  state = {
-      time: new Date('1985-01-03'),
-      isOpen: true,
+
+const AgeGate = (({ onClick = () => void (0), text, buttonStyles }: IAgeGateProps): JSX.Element => {
+  const [time, setTime] = useState(new Date())
+  const [isOpen, setIsOpen] = useState(true)
+  console.log(time)
+  const monthMap = {
+    '1': 'Jan',
+    '2': 'Feb',
+    '3': 'Mar',
+    '4': 'Apr',
+
+    '5': 'May',
+    '6': 'Jun',
+    '7': 'Jul',
+    '8': 'Aug',
+    '9': 'Sep',
+    '10': 'Oct',
+    '11': 'Nov',
+    '12': 'Dec',
   }
 
-  handleClick = () => {
-      this.setState({ isOpen: true })
+  const dateConfig = {
+    'month': {
+      format: value => monthMap[value.getMonth() + 1],
+      caption: 'Mon',
+      step: 1,
+    },
+    'date': {
+      format: 'DD',
+      caption: 'Day',
+      step: 1,
+    },
+    'year': {
+      format: 'YYYY',
+      caption: 'Year',
+      step: 1,
+    },
   }
 
-  handleCancel = () => {
-      this.setState({ isOpen: false })
-  }
 
-  handleSelect = (time) => {
-      this.setState({ time, isOpen: false })
-  }
-
-  render() {
-    const monthMap = {
-      '1': 'Jan',
-      '2': 'Feb',
-      '3': 'Mar',
-      '4': 'Apr',
-      '5': 'May',
-      '6': 'Jun',
-      '7': 'Jul',
-      '8': 'Aug',
-      '9': 'Sep',
-      '10': 'Oct',
-      '11': 'Nov',
-      '12': 'Dec',
-    }
-
-    const dateConfig = {
-      'month': {
-          format: value => monthMap[value.getMonth() + 1],
-          caption: 'Mon',
-          step: 1,
-      },
-      'date': {
-        format: 'DD',
-        caption: 'Day',
-        step: 1,
-      },
-      'year': {
-        format: 'YYYY',
-        caption: 'Year',
-        step: 1,
-      },
-    }
-
-    return (
-      <Container className={css(commonStyles.flexRowAllCenter)}>
-        <Row className={css(commonStyles.flexRowAllCenter)}>
-          <p className={css(styles.title)}>
-            {'Please select your birthdate:'}
-          </p>
-        </Row>
-        <Row className={css(commonStyles.flexRowAllCenter, styles.datePickerRow)}>
-          <DatePicker
-            theme='android'
-            dateConfig={dateConfig}
-            showHeader={false}
-            showFooter={false}
-            value={this.state.time}
-            isOpen={this.state.isOpen}
-            onSelect={this.handleSelect}
-            onCancel={this.handleCancel}
-            isPopup={false}
-            max={new Date(Date.now() - 86400000)}
-            min={new Date('1900-01-02')}
-          />
-        </Row>
-        <Row className={css(commonStyles.flexRowAllCenter, styles.buttonDiv)}>
-          <Button onClick={handleClick} text='Submit' />
-        </Row>
-      </Container>
-    )
-  }
-}
+  return (
+    <Container className={css(commonStyles.flexColumnAlignCenter)}>
+      <Row className={css(commonStyles.flexRowAllCenter)}>
+        <p className={css(styles.title)}>
+          {'Please select your birthdate:'}
+        </p>
+      </Row>
+      <Row className={css(commonStyles.flexRowAllCenter, styles.datePickerRow)}>
+        <DatePicker
+          theme='android'
+          showCaption={isOpen}
+          dateConfig={dateConfig}
+          showHeader={false}
+          showFooter={false}
+          onSelect={() => { console.log('select') }}
+          onCancel={() => { console.log('cancel') }}
+          value={time}
+          isOpen={true}
+          isPopup={false}
+          max={new Date(Date.now() - 86400000)}
+          min={new Date('1900-01-02')}
+        />
+      </Row>
+      <Row className={css(commonStyles.flexRowAllCenter, styles.buttonDiv)}>
+        <Button onClick={handleClick} text='Submit' />
+      </Row>
+    </Container>
+  )
+})
 
 export default AgeGate
