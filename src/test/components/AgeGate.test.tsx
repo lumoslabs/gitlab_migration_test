1c00d347f39476cb66d67523ed7257e5c15ea3a5
Liinal <liina@lumoslabs.com>
2021-08-17 16:08:36 -0700

    age gate snap fix

--- src/test/components/AgeGate.test.tsx
+++ src/test/components/AgeGate.test.tsx
@@ -8,8 +8,8 @@ const onSubmit = () => { window.location.href = '/' }
 it('matches snapshot', () => {
   const { asFragment } = render(
     <AgeGate
-      max={new Date(Date.UTC(2021, 7, 16, 17, 33, 0, 0))}
-      min={new Date(Date.UTC(1900, 7, 16, 17, 33, 0, 0))}
+      max={new Date(Date.UTC(2021, 1, 1, 1, 1, 0, 0))}
+      min={new Date(Date.UTC(1900, 1, 1, 1, 1, 0, 0))}
       onSubmit={onSubmit}
     />
   )
