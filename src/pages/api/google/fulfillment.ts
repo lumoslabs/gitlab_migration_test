919189b773047c0569327d54fc290d632497bcf2
Liinal <liina@lumoslabs.com>
2022-01-28 14:14:50 -0800

    rollbar for amplitude user ID problem

--- src/pages/api/google/fulfillment.ts
+++ src/pages/api/google/fulfillment.ts
@@ -81,7 +81,7 @@ conversationApp.handle('Next', onNext)
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   try {
     logger.debug(`Fulfillment Request ${req.body?.handler?.name}`)
-
+    rollbar?.info(`request from user_id: ${req.body?.user?.params?.id} data: ${req.body}`)
     amplitudeBackendEvent({
       eventName: `intent_${req.body?.handler?.name}`,
       userId: req.body?.user?.params?.id,
